5aca03c6bfda2fd5ecbcb6c59fa320dc45c8211c
Anupheaus <tony.hales@moodinternational.com>
2019-04-11 11:08:01 +0100

    Fixed removing node modules when target is node

--- configs/webpack/externals.js
+++ configs/webpack/externals.js
@@ -1,20 +1,16 @@
-const path = require('path');
-const fs = require('fs');
-const nodeExternals = require('webpack-node-externals');
+function nodeExternals(context, request, callback) {
+  var moduleName = context.toLowerCase();
+  if (moduleName.includes('node_modules')) {
+    callback(null, `commonjs ${request}`);
+  } else {
+    callback();
+  }
+};
 
-function addNodeModules(options) {
-  if (options.target !== 'node') { return; }
-  const packageNodeModules = path.resolve(options.root, 'node_modules');
-  const rootNodeModules = path.resolve(__dirname, '../node_modules');
-  return fs.readdirSync(rootNodeModules)
-    .concat(fs.readdirSync(packageNodeModules))
-    .filter(x => ['.bin'].indexOf(x) === -1)
-    .reduce((externals, mod) => ({
-      ...externals,
-      [mod]: 'commonjs ' + mod,
-    }), {});
-}
 module.exports = function externals(options) {
   if (options.target !== 'node') { return undefined; } // needs to be undefined or webpack config won't work
-  return [nodeExternals()];  
+  return [
+    nodeExternals, 
+    ...(options.externals || []),
+  ];
 };
