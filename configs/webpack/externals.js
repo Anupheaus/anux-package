0ae0a37d2983168310208f78c74ce6bc72896049
Anupheaus <tony.hales@moodinternational.com>
2019-04-13 20:05:57 +0100

    Added back nodeExternals

--- configs/webpack/externals.js
+++ configs/webpack/externals.js
@@ -1,15 +1,17 @@
-function nodeExternals(context, request, callback) {
-  var moduleName = context.toLowerCase();
-  if (moduleName.includes('node_modules')) {
-    callback(null, `commonjs ${request}`);
-  } else {
-    callback();
-  }
-};
+const nodeExternals = require('webpack-node-externals');
+
+// function nodeExternals(context, request, callback) {
+//   var moduleName = context.toLowerCase();
+//   if (moduleName.includes('node_modules')) {
+//     callback(null, `commonjs ${request}`);
+//   } else {
+//     callback();
+//   }
+// };
 
 module.exports = function externals(options) {
   return [
-    options.excludeNodeModules ? nodeExternals : null,
+    options.excludeNodeModules ? nodeExternals() : null,
     ...(options.externals || []),
   ].filter(v => !!v);
 };
